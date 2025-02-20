0e9f455caad98de4488032be086cdebcd274b7e2
OkunorenBoluwatife <okunorenboluwatife@gmail.com>
2025-02-20 18:48:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+dict = {1,2,2,3,4,4,4}
+print("Set :", dict)
+
+dict.add(5)
+print("Updated Set:", dict)
+
+dict.remove(3)
+print("Set after removing 3:", dict)
+
+dict.clear()
+print("Set after clearing:", dict)
